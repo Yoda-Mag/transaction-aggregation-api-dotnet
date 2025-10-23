5cac4cc6d1d9ddf142fcf787542ee1f52848df7f
Yoda-Mag <maganotlhogie@gmail.com>
2025-10-23 20:50:36 +0200

    Customers have surnames

--- ProjectsTransactionAggregation/TransactionAggregationApi/Data/MockDataSource.cs
+++ ProjectsTransactionAggregation/TransactionAggregationApi/Data/MockDataSource.cs
@@ -6,9 +6,10 @@ namespace TransactionAggregationApi.Data
     {
         public static List<Customer> Customers = new List<Customer>
         {
-            new Customer { Id = Guid.NewGuid(), Name = "Alice", Email = "Alice@theBoaderland.com" },
-            new Customer { Id = Guid.NewGuid(), Name = "Bob", Email = "Bobby@icloud.com" },
-            new Customer { Id = Guid.NewGuid(), Name = "Charlie" }
+            //Adding instance of customers
+            new Customer { Id = Guid.NewGuid(), Name = "Alice",Surname = "Just", Email = "Alice@theBoaderland.com" },
+            new Customer { Id = Guid.NewGuid(), Name = "Bob",Surname = "Fischer", Email = "Bobby@icloud.com" },
+            new Customer { Id = Guid.NewGuid(), Name = "Charlie",Surname = "Brooker", Email = "CharlieBrooker@gmail.com"}
         };
 
         public static List<Transaction> Transactions = new List<Transaction>();
